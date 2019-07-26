6c93471bd5d0d25e87a91afcc96c114ad83ad052
Dimas Adiyaksa <hello@dimasadyaksa.my.id>
2019-07-26 15:48:04 +0700

    initial commit

---
+++ App.js
@@ -0,0 +1,22 @@
+const App   = require('express')()
+const http  = require('http').Server(App)
+const io    = require('socket.io')(http)
+const port  = process.env.PORT || 5000
+
+App.get('/', function(req, res){
+    res.sendFile(__dirname + '/View.html')
+})
+  
+//io.set('origins', 'http://localhost:5000');
+io.on('connection', function(socket){
+    console.log('user connected')
+    socket.on('disconnect',function(){
+        console.log('user disconnected')
+    })
+    socket.on('chat message',function(msg){
+        io.emit('chat message',msg)
+    })
+})
+http.listen(port,function(){
+    console.log('listen on port : '+port)
+})
